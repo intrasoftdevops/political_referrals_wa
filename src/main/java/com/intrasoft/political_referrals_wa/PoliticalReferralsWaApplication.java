9d3e06cb424035d39ad6dbe2c2ab4f8578f6300e
Intrasoft <intrasoftdevops@gmail.com>
2025-07-03 15:59:29 -0500

    Initial commit

---
+++ src/main/java/com/intrasoft/political_referrals_wa/PoliticalReferralsWaApplication.java
@@ -0,0 +1,13 @@
+package com.intrasoft.political_referrals_wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalReferralsWaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalReferralsWaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intrasoft/political_referrals_wa/PoliticalReferralsWaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intrasoft.political_referrals_wa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalReferralsWaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
